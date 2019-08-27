31282b00265eeac6eabfec6ee699cbda9b22c704
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-08-27 22:47:16 +0100

    init++

---
+++ build_client/precache-manifest.21ac8849522d7720baa68f5d573dd312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b51316774ccedad81c2a427c703f22ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7cc43863471f846e11a",
+    "url": "/static/css/main.5a3a32e1.chunk.css"
+  },
+  {
+    "revision": "9ac03d393fc2a71c26a4",
+    "url": "/static/js/2.67917007.chunk.js"
+  },
+  {
+    "revision": "a7cc43863471f846e11a",
+    "url": "/static/js/main.5eb386d0.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  },
+  {
+    "revision": "dc08c20237d03631a31014beada12ce6",
+    "url": "/static/media/close.dc08c202.png"
+  }
+]);
