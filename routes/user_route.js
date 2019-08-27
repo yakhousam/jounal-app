e82c2866aa4e8052055cc3bf81d8955edd61ea3a
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-08-27 22:41:33 +0100

    init++

--- routes/user_route.js
+++ routes/user_route.js
@@ -10,8 +10,8 @@ router.put(
     // console.log("body =", req.body);
     try {
       const { username, password } = req.body;
-      if(!username) return res.status(400).json({ error: "Username is empty" });
-      if(!password || password.length < 8) return res.status(400).json({ error: "Password must be at least 8 carracter long" });
+      if (!username) return res.status(400).json({ error: "Username is empty" });
+      if (!password || password.length < 8) return res.status(400).json({ error: "Password must be at least 8 carracter long" });
       // console.log(username, password);
       const user = await User.findOne({ "local.username": username });
       if (user) return res.status(400).json({ error: "Username already used" });
@@ -23,31 +23,31 @@ router.put(
       res.status(500).json({ error: "Internal server error" });
     }
   },
-  passport.authenticate("local"), (req, res)=> res.status(201).json({userId: req.user._id})
+  passport.authenticate("local"), (req, res) => res.status(201).json({ userId: req.user._id })
   // passport.authenticate("local"), (req, res)=> res.redirect('/')
 );
 
 router.post(
   "/login",
-  passport.authenticate("local"), (req, res)=> res.status(200).json({userId: req.user._id})
+  passport.authenticate("local"), (req, res) => res.status(200).json({ userId: req.user._id })
   // passport.authenticate("local"), (req, res)=> res.redirect('/')
 );
 
-router.get('/getUserInfos/', async (req, res)=>{
+router.get('/getUserInfos/', async (req, res) => {
   try {
     const id = req.user._id;
     //  console.log("get user info by id =", id)
     const user = await User.findById(id);
-    if(user){
-      if(user.local){
-        return res.status(200).json({username: user.local.username || user.social.github.username})
-      }else{
-        res.status(400).json({error: 'User not found'})
+    if (user) {
+      if (user.local) {
+        return res.status(200).json({ username: user.local.username || user.social.github.username })
+      } else {
+        res.status(400).json({ error: 'User not found' })
       }
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({error: 'server error'})
+    res.status(500).json({ error: 'server error' })
   }
 })
 
@@ -55,11 +55,7 @@ router.get('/getUserInfos/', async (req, res)=>{
 router.get('/auth/github',
   passport.authenticate('github'));
 
-  router.get('/auth/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/auth' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-});
+router.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/auth', successRedirect: '/' }));
 
 module.exports = router;
